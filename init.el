bdd1709067f7e406ea45a79a2f5f2f004ed044a4
lithium <lithium@tsunagatte.ru>
2023-02-08 21:26:57 +0500

    Font changed

--- init.el
+++ init.el
@@ -45,20 +45,11 @@
  '(package-selected-packages
    '(ghub treemacs-magit magit company-reftex company-auctex auctex yaml-mode json-mode go-mode helpful slime lsp-pyright cmake-mode ccls sqlite3 web-mode company-prescient company-box company lsp-treemacs lsp-ui lsp-mode flycheck yasnippet projectile ido-vertical-mode treemacs-icons-dired treemacs-evil treemacs undo-tree page-break-lines async switch-window beacon swiper which-key dashboard spaceline diminish auto-package-update multi-vterm vterm evil-org htmlize use-package))
  '(warning-suppress-types '((comp))))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "#000000" :foreground "#ffffff" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "CTDB" :family "Iosevka Comfy"))))
- '(company-tooltip-selection ((t (:inherit company-tooltip-selection :extend t))))
- '(fixed-pitch ((t (:family "Iosevka Comfy"))))
- '(homoglyph ((t (:foreground "cornflower blue"))))
- '(nobreak-hyphen ((t (:foreground "cornflower blue"))))
- '(org-level-1 ((t (:inherit outline-1 :height 1.5))))
- '(org-level-2 ((t (:inherit outline-2 :height 1.3))))
- '(org-level-3 ((t (:inherit outline-3 :height 1.1))))
- '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
- '(org-level-5 ((t (:inherit outline-5 :height 1.0))))
- '(variable-pitch ((t (:family "Iosevka Comfy")))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 122 :width normal :foundry "CTDB" :family "Iosevka Comfy Fixed")))))
 
