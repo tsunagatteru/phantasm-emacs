0acf1d6bae262a5699a954296d3153b10c1f3315
lithium <lithium@tsunagatte.ru>
2023-02-14 21:21:26 +0500

    Removed straight and reordered init

--- init.el
+++ init.el
@@ -14,22 +14,6 @@
  
 (add-hook 'emacs-startup-hook 'startup/revert-file-name-handler-alist)
 (add-hook 'emacs-startup-hook 'startup/reset-gc)
-;;
-
-;;Initialize straight
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 6))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
-(setq package-enable-at-startup nil)
 
 ;; Initialize melpa repo
 (require 'package)
@@ -47,20 +31,6 @@
 (require 'use-package-ensure)
   (setq use-package-always-ensure t)
 
-;; Load Witchmacs theme
-(load-theme 'Witchmacs t)
-
-;; Load config.org for init.el configuration
-(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(ghub treemacs-magit magit company-reftex company-auctex auctex yaml-mode json-mode go-mode helpful slime lsp-pyright cmake-mode ccls sqlite3 web-mode company-prescient company-box company lsp-treemacs lsp-ui lsp-mode flycheck yasnippet projectile ido-vertical-mode treemacs-icons-dired treemacs-evil treemacs undo-tree page-break-lines async switch-window beacon swiper which-key dashboard spaceline diminish auto-package-update multi-vterm vterm evil-org htmlize use-package))
- '(warning-suppress-types '((comp))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -69,3 +39,8 @@
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :extend nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 122 :width normal :foundry "SRC" :family "Hack Nerd Font Mono")))))
 
+;; Load Witchmacs theme
+(load-theme 'Witchmacs t)
+
+;; Load config.org for init.el configuration
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
