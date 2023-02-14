df87402251a5ddade1c16608222a2aa9724574b5
lithium <lithium@tsunagatte.ru>
2023-02-14 21:43:00 +0500

    Custom variables and straight line removed

--- init.el
+++ init.el
@@ -44,3 +44,10 @@
 
 ;; Load config.org for init.el configuration
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(ghub treemacs-magit magit company-reftex company-auctex auctex yaml-mode json-mode go-mode helpful slime lsp-pyright cmake-mode ccls sqlite3 web-mode company-prescient company-box company lsp-treemacs lsp-ui lsp-mode flycheck yasnippet projectile ido-vertical-mode treemacs-icons-dired treemacs-evil treemacs undo-tree page-break-lines async switch-window beacon swiper which-key dashboard spaceline diminish auto-package-update vterm evil-org htmlize use-package)))
