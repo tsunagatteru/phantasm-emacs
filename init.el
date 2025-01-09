1645b8eac9b8ad1c7d6aa30e0d7f740cfdd886e8
lithium <lithium@tsunagatte.ru>
2025-01-09 16:49:30 +0500

    update init.el

--- init.el
+++ init.el
@@ -46,3 +46,10 @@
 ;; Load config.org for init.el configuration
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(highlight-indent-guides ghub yaml-mode which-key web-mode use-package undo-tree treepy treemacs-magit treemacs-icons-dired treemacs-evil toc-org terraform-mode systemd switch-window swiper sqlite3 spaceline slime ranger pyvenv projectile powershell page-break-lines nginx-mode multi-vterm lsp-ui lsp-treemacs lsp-pyright json-mode jenkinsfile-mode ido-vertical-mode htmlize helpful go-mode geiser-mit flycheck evil-org dockerfile-mode diminish dashboard csharp-mode company-reftex company-prescient company-box company-auctex cmake-mode ccls beacon auto-package-update async)))
