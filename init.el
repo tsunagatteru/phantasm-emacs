c559d1b473f7cff2a5fd7252438179f436d89c98
lithium <lithium@tsunagatte.ru>
2025-02-21 08:59:40 +0500

    Change font size

--- init.el
+++ init.el
@@ -37,7 +37,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 100 :width normal :foundry "SRC" :family "Hack Nerd Font Mono")))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 230 :width normal :foundry "SRC" :family "Hack Nerd Font Mono")))))
 
 ;; Load Witchmacs theme
 (load-theme 'Witchmacs t)
